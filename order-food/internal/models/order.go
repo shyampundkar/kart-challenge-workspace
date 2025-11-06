1212a54145b41e6db6c33cfff4cdfddaa4ec2789
shyampundkar <shyam.pundkar@gmail.com>
2025-11-06 20:47:16 +1100

    added local ci

--- order-food/internal/models/order.go
+++ order-food/internal/models/order.go
@@ -14,7 +14,7 @@ type OrderReq struct {
 
 // Order represents a completed order
 type Order struct {
-	ID       string     `json:"id"`
+	ID       string      `json:"id"`
 	Items    []OrderItem `json:"items"`
 	Products []Product   `json:"products"`
 }
